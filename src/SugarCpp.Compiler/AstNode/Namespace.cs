dad51ba1aeab7ed1f1fc06218aa565ddeb433b48
curimit <curimit@gmail.com>
2013-04-29 15:53:39 +0800

    Add namespace.cs.

---
+++ src/SugarCpp.Compiler/AstNode/Namespace.cs
@@ -0,0 +1,28 @@
+﻿using Antlr4.StringTemplate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SugarCpp.Compiler
+{
+    public class Namespace : AstNode
+    {
+        public string Name;
+        public List<AstNode> List = new List<AstNode>();
+
+        public Namespace(string name, List<AstNode> list)
+        {
+            this.Name = name;
+            if (list != null)
+            {
+                this.List = list;
+            }
+        }
+
+        public override Template Accept(Visitor visitor)
+        {
+            return visitor.Visit(this);
+        }
+    }
+}
