e6cbd09032cc96e8b4e26261522a86dd36c36194
curimit <curimit@gmail.com>
2013-04-30 11:10:29 +0800

    Add SugarCpp.WinForm.

---
+++ src/SugarCpp.WinForm/MainWindow.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SugarCpp.WinForm
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Source = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Source);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.Result);
+            this.splitContainer1.Size = new System.Drawing.Size(877, 533);
+            this.splitContainer1.SplitterDistance = 373;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Source
+            // 
+            this.Source.AcceptsReturn = true;
+            this.Source.AcceptsTab = true;
+            this.Source.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Source.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Source.Location = new System.Drawing.Point(3, 3);
+            this.Source.Multiline = true;
+            this.Source.Name = "Source";
+            this.Source.Size = new System.Drawing.Size(367, 527);
+            this.Source.TabIndex = 0;
+            this.Source.TextChanged += new System.EventHandler(this.Source_TextChanged);
+            // 
+            // Result
+            // 
+            this.Result.AcceptsReturn = true;
+            this.Result.AcceptsTab = true;
+            this.Result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Result.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result.Location = new System.Drawing.Point(3, 3);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(494, 527);
+            this.Result.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 533);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox Source;
+        private System.Windows.Forms.TextBox Result;
+    }
+}
+
---
+++ src/SugarCpp.WinForm/MainWindow.cs
@@ -0,0 +1,44 @@
+﻿using SugarCpp.Compiler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SugarCpp.WinForm
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (File.Exists("test.sc"))
+            {
+                this.Source.Text = File.ReadAllText("test.sc");
+            }
+        }
+
+        private void Source_TextChanged(object sender, EventArgs e)
+        {
+            string input = this.Source.Text.Replace("\t", "    ");
+            File.WriteAllText("test.sc", input);
+            try
+            {
+                TargetCpp sugar_cpp = new TargetCpp();
+                string output = sugar_cpp.Compile(input);
+                this.Result.Text = output;
+                File.WriteAllText("test.cpp", output);
+            }
+            catch (Exception ex)
+            {
+                string output = string.Format("Compile Error:\n{0}", ex.Message);
+                this.Result.Text = output;
+            }
+        }
+    }
+}
