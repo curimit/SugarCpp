b626ec32717b7656ae6006c5ea9e9b18869d4aac
curimit <curimit@gmail.com>
2013-05-02 09:49:33 +0800

    Fix bug on hexadecimal numbers.

--- src/SugarCpp.Compiler/Antlr/SugarCppLexer.cs
+++ src/SugarCpp.Compiler/Antlr/SugarCppLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.4 SugarCpp.g 2013-05-02 09:45:19
+// $ANTLR 3.4 SugarCpp.g 2013-05-02 09:48:47
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
