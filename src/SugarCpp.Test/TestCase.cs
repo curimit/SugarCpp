b9d52b0411ff2e16fa3e97cecd8a3a68eb510fb2
curimit <curimit@gmail.com>
2013-05-28 13:06:28 +0800

    Remove test case.

--- src/SugarCpp.Test/TestCase.cs
+++ src/SugarCpp.Test/TestCase.cs
@@ -8,12 +8,14 @@ namespace SugarCpp.Test
     internal class TestCase
     {
         public string Input { get; set; }
-        public string Result { get; set; }
+        public string Header { get; set; }
+        public string Implementation { get; set; }
 
-        public TestCase(string input, string result)
+        public TestCase(string input, string header, string impelementation)
         {
             this.Input = input;
-            this.Result = result;
+            this.Header = header;
+            this.Implementation = impelementation;
         }
     }
 }
